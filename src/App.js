fc44a9d607c0e5e85146224d421f686d8e0fd90c
Richard Lynch <rlynch2012@gmail.com>
2024-05-22 22:42:20 -0400

    add initial html for homepage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
 const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: "Hats",
+    },
+    {
+      id: 2,
+      title: "Jackets",
+    },
+    {
+      id: 3,
+      title: "Sneakers",
+    },
+    {
+      id: 4,
+      title: "Womens",
+    },
+    {
+      id: 5,
+      title: "Mens",
+    },
+  ];
+
   return (
-    <div className="App">
-      Hello world
+    <div className="categories-container">
+      {categories.map(({ title, id }) => (
+        <div className="category container">
+          {/* <img/> */}
+          <div className="category-body-container" key={id}>
+            <h2>{title}</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
