f9fff8a7afb4b65b09623bb060e5eb6fe35269c8
Richard Lynch <rlynch2012@gmail.com>
2024-05-24 04:11:51 -0400

    add tailwindcss to categories-containers

--- src/App.js
+++ src/App.js
@@ -23,12 +23,12 @@ const App = () => {
   ];
 
   return (
-    <div className="categories-container">
+    <div className="flex flex-wrap size-full justify-between">
       {categories.map(({ title, id }) => (
-        <div className="category container" key={id}>
+        <div className="min-w-32 h-60 flex-auto" key={id}>
           <div className="background-image"/>
           <div className="category-body-container">
-            <h2 className="text-3xl font-bold underline bg-red-800">{title}</h2>
+            <h2 className="text-3xl font-bold underline">{title}</h2>
             <p>Shop Now</p>
           </div>
         </div>
