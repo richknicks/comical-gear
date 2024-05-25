a5354b943388703d6e3ce2d690b51f44dbf3837f
Richard Lynch <rlynch2012@gmail.com>
2024-05-25 02:21:06 -0400

    styled framework for homepage

--- src/App.js
+++ src/App.js
@@ -23,13 +23,33 @@ const App = () => {
   ];
 
   return (
-    <div className="flex flex-wrap size-full justify-between">
+    <div
+      id="categories-container"
+      className="flex flex-wrap size-full justify-between"
+    >
       {categories.map(({ title, id }) => (
-        <div className="min-w-32 h-60 flex-auto" key={id}>
-          <div className="background-image"/>
-          <div className="category-body-container">
-            <h2 className="text-3xl font-bold underline">{title}</h2>
-            <p>Shop Now</p>
+        <div
+          id="category-container"
+          className="min-w-32 h-60 flex flex-auto items-center justify-center border border-black mt-0 mx-8 mb-16 overflow-hidden hover:cursor-pointer"
+          key={id}
+        >
+          <div
+            id="background-image"
+            className="scale-110 transition-transform duration-600 w-full h-full bg-cover bg-center"
+          />
+          <div
+            id="category-body-container"
+            className="opacity-90 h-90 py-0 px-2 flex flex-col items-center justify-center border border-black bg-white absolute"
+          >
+            <h2
+              id="category-name"
+              className="font-bold mt-0 mx-2 mb-0 text-22 text-gray-700"
+            >
+              {title}
+            </h2>
+            <p id="call-to-action" className="font-light text-16">
+              Shop Now
+            </p>
           </div>
         </div>
       ))}
