8d5379eef73610faf792cb801f80b82c4a71b252
Richard Lynch <rlynch2012@gmail.com>
2024-05-29 21:10:29 -0400

    add photo assets and styled with tailwind

--- src/App.js
+++ src/App.js
@@ -3,43 +3,45 @@ const App = () => {
     {
       id: 1,
       title: "Hats",
+      imageUrl: `${process.env.PUBLIC_URL}/images/bat-logo.png`,
     },
     {
       id: 2,
       title: "Jackets",
+      imageUrl: `${process.env.PUBLIC_URL}/images/Gnh-skull.png`,
     },
     {
       id: 3,
       title: "Sneakers",
+      imageUrl: `${process.env.PUBLIC_URL}/images/Jordancry.png`,
     },
     {
       id: 4,
-      title: "Womens",
+      title: "Mens",
+      imageUrl: `${process.env.PUBLIC_URL}/images/Knicks.png`,
     },
     {
       id: 5,
-      title: "Mens",
+      title: "Womens",
+      imageUrl: `${process.env.PUBLIC_URL}/images/Sesame-street.png`,
     },
   ];
 
   return (
     <div
       id="categories-container"
-      className="flex flex-wrap size-full justify-between"
+      className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 mt-12"
     >
-      {categories.map(({ title, id }) => (
+      {categories.map(({ title, id, imageUrl }) => (
         <div
           id="category-container"
-          className="min-w-32 h-60 flex flex-auto items-center justify-center border border-black mt-0 mx-8 mb-16 overflow-hidden hover:cursor-pointer"
+          className="min-w-32 h-52 flex flex-auto items-center justify-center border border-black rounded-3xl shadow-lg mt-0 mx-1 mb-4 overflow-hidden hover:cursor-pointer"
           key={id}
         >
+          <img src={`${imageUrl}`} className="w-full h-auto"/>
           <div
-            id="background-image"
-            className="scale-110 transition-transform duration-600 w-full h-full bg-cover bg-center"
-          />
-          <div
-            id="category-body-container"
-            className="opacity-90 h-90 py-0 px-2 flex flex-col items-center justify-center border border-black bg-white absolute"
+            id="category-label-container"
+            className=" opacity-70 h-90 py-0 px-2 mt-40 flex flex-col item justify-center border border-black bg-white absolute"
           >
             <h2
               id="category-name"
