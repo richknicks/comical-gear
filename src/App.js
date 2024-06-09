edbc2028d9963716bdde6465ffc34edac627ec7d
Richard Lynch <rlynch2012@gmail.com>
2024-06-08 23:22:50 -0400

    add component for category

--- src/App.js
+++ src/App.js
@@ -1,29 +1,36 @@
+import CategoryItem from "./components/category-item/category-item.component";
+
 const App = () => {
   const categories = [
     {
       id: 1,
       title: "Hats",
-      imageUrl: `${process.env.PUBLIC_URL}/images/bat-logo.png`,
+      imageUrl: `https://placeit.net/uploads/preview_image/136577/image/large_thumb_override_image_4743d5b095d888817a4e2f22fc6dc4aa.jpg`,
+      // imageUrl: `${process.env.PUBLIC_URL}/images/bat-logo.png`,
     },
     {
       id: 2,
       title: "Jackets",
-      imageUrl: `${process.env.PUBLIC_URL}/images/Gnh-skull.png`,
+      imageUrl: `https://placeit.net/uploads/preview_image/135633/image/large_thumb_override_image_0022fd4fe54d76e0131c9ac0edcc3241.jpg`,
+      // imageUrl: `${process.env.PUBLIC_URL}/images/Gnh-skull.png`,
     },
     {
       id: 3,
       title: "Sneakers",
-      imageUrl: `${process.env.PUBLIC_URL}/images/Jordancry.png`,
+      imageUrl: `https://placeit.net/uploads/preview_image/135368/image/large_thumb_override_image_672a17be764182b3bb061a4aeb8c78de.jpg`,
+      // imageUrl: `${process.env.PUBLIC_URL}/images/Jordancry.png`,
     },
     {
       id: 4,
       title: "Mens",
-      imageUrl: `${process.env.PUBLIC_URL}/images/Knicks.png`,
+      imageUrl: `https://placeit.net/uploads/preview_image/136648/image/large_thumb_override_image_ae0586ac7652a9676555e375a7ded225.jpg`,
+      // imageUrl: `${process.env.PUBLIC_URL}/images/Knicks.png`,
     },
     {
       id: 5,
       title: "Womens",
-      imageUrl: `${process.env.PUBLIC_URL}/images/Sesame-street.png`,
+      imageUrl: `https://placeit.net/uploads/preview_image/135458/image/large_thumb_override_image_040f088e9efd97a958a35ee8570d2d71.jpg`,
+      // imageUrl: `${process.env.PUBLIC_URL}/images/Sesame-street.png`,
     },
   ];
 
@@ -32,28 +39,8 @@ const App = () => {
       id="categories-container"
       className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 mt-12"
     >
-      {categories.map(({ title, id, imageUrl }) => (
-        <div
-          id="category-container"
-          className="min-w-32 h-52 flex flex-auto items-center justify-center border border-black rounded-3xl shadow-lg mt-0 mx-1 mb-4 overflow-hidden hover:cursor-pointer"
-          key={id}
-        >
-          <img src={`${imageUrl}`} className="w-full h-auto"/>
-          <div
-            id="category-label-container"
-            className=" opacity-70 h-90 py-0 px-2 mt-40 flex flex-col item justify-center border border-black bg-white absolute"
-          >
-            <h2
-              id="category-name"
-              className="font-bold mt-0 mx-2 mb-0 text-22 text-gray-700"
-            >
-              {title}
-            </h2>
-            <p id="call-to-action" className="font-light text-16">
-              Shop Now
-            </p>
-          </div>
-        </div>
+      {categories.map(( category ) => (
+         <CategoryItem key={ category.id } category={ category }/>
       ))}
     </div>
   );
